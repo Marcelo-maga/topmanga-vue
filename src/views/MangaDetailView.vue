f80c448219f5ef5a3c496e5a2f1a583e45a60ccf
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2023-09-26 22:35:40 -0300

    hotfix: manga details bug

--- src/views/MangaDetailView.vue
+++ src/views/MangaDetailView.vue
@@ -3,23 +3,33 @@ import { onMounted, reactive, ref } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
-let manga = reactive(ref());
+let manga = reactive(ref({
+  title: "",
+  images: {
+    jpg: {
+      image_url: ""
+    }
+  },
+  score: 0,
+  synopsis: ""
+}));
 
 onMounted(() => {
     fetch(`https://api.jikan.moe/v4/manga/${route.params.id}`)
         .then((response) => response.json())
         .then(response => {
             manga.value = response.data
+            console.log(manga.value)
     })
 })
 </script>
 
+
 <template>
     <div class="">
         <div class="container p-4">
-
             <div class="bg-zinc-800 rounded p-4 flex gap-4">
-                <img :src="manga.images.jpg.image_url" alt="Manga Cover" class=" rounded-lg mb-4">
+                <img v-if="manga.images && manga.images.jpg" :src="manga.images.jpg.image_url" alt="Manga Cover" class="rounded-lg mb-4">
                 <div>
                     <h1 class="text-3xl text-zinc-100 font-bold mb-2">{{ manga.title }}</h1>
     
@@ -29,9 +39,7 @@ onMounted(() => {
     
                     <p class="text-zinc-100 mb-4">{{ manga.synopsis }}</p>
                 </div>
-
             </div>
         </div>
     </div>
 </template>
-  
